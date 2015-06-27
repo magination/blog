91d4474bbf03f8a4cd833527d6e94caa1e007583
Juul Arthur <juularthur92@gmail.com>
2015-06-27 17:05:56 +0200

    fix init script

--- updates/init.js
+++ updates/init.js
@@ -8,39 +8,45 @@ var initPosts = [
   {
     title:'testpost1-title',
     body: 'testpost1-body',
+    slug: 'testpost1-title',
     author: initUser
   },
   {
     title:'testpost2-title',
     body: 'testpost2-body',
+    slug: 'testpost2-title',
     author: initUser
   },
   {
     title:'testpost3-title',
     body: 'testpost3-body',
+    slug: 'testpost3-title',
     author: initUser
   },
   {
     title:'testpost4-title',
     body: 'testpost4-body',
+    slug: 'testpost4-title',
     author: initUser
   },
   {
     title:'testpost5-title',
     body: 'testpost5-body',
+    slug: 'testpost5-title',
     author: initUser
   },
 ];
 
 Post.find().exec(function (err, posts){
     if (posts.length == 0){
-      Post.create(initPosts);
+      Post.create(initPosts, function (err) {
+        console.log(err);
+      })
       console.log('Created initial posts');
     }
 });
 User.find().exec(function (err, user){
   if (user.length == 0){
     initUser.save();
-    console.log('Created initial user');
   };
 });
