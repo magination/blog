7d466a93ef294d70bcc10dd3b0a63aaaebb9e9fd
Hans Kristian <hans.k.henriksen@bekk.no>
2015-07-08 19:28:11 +0200

    Fix broken links from frontview to blogposts

--- frontend/app/components/PreviewBlogComponent.js
+++ frontend/app/components/PreviewBlogComponent.js
@@ -6,7 +6,7 @@ var previewBlogComponent = React.createClass({
     render: function(){
         return(
             <div className="blogPostPreview">
-                <h2> <Link to={`/post/${this.props.blogpost.title}`}>{this.props.blogpost.title} </Link> </h2>
+                <h2> <Link to={`/post/${this.props.blogpost.slug}`}>{this.props.blogpost.title} </Link> </h2>
                 <div className="author"><p> Written by: <span className="authorName"> Juul Arthur </span> </p></div>
                 <p> {this.props.blogpost.body} </p>
                 <button name="readMoreButton">Read more</button>
