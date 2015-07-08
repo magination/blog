3d789c0d22fc76d733fb14d2b90518d3725fa2fa
Hans Kristian <hans.k.henriksen@bekk.no>
2015-07-08 19:47:37 +0200

    Add link to buttons

--- frontend/app/components/PreviewBlogComponent.js
+++ frontend/app/components/PreviewBlogComponent.js
@@ -9,7 +9,7 @@ var previewBlogComponent = React.createClass({
                 <h2> <Link to={`/post/${this.props.blogpost.slug}`}>{this.props.blogpost.title} </Link> </h2>
                 <div className="author"><p> Written by: <span className="authorName"> Juul Arthur </span> </p></div>
                 <p> {this.props.blogpost.body} </p>
-                <button name="readMoreButton">Read more</button>
+                <Link to={`/post/${this.props.blogpost.slug}`}><button name="readMoreButton">Read more</button></Link>
             </div>
         )
     }
