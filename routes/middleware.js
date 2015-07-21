86f7585f5d2f8096d72e38b4a594e6412ad36476
Juul Arthur <juularthur92@gmail.com>
2015-07-21 19:53:14 +0200

    remove more console logs

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,12 +1,10 @@
 
 
 module.exports.requireUser = function(req,res,next){
-	console.log("authenticating");
-	console.log(req.user);
   if(!req.user) {
     res.status(401);
     return res.send({message: 'Unauthorized, please log in', status: 401});
   } 
-  
+
   next();
 };
