3cd95c60cb2f1ae546f8270fa2ff1aef8686a13f
tiborv <tiborvukovic91@gmail.com>
2015-07-29 10:47:15 +0200

    env: Check for production env on startup

--- server.js
+++ server.js
@@ -3,18 +3,25 @@ var session 		= require('express-session')
 var bodyParser      = require('body-parser');
 var router          = require('./routes');
 var mongoose        = require('mongoose');
-mongoose.connect('mongodb://localhost/blog');
 var passport        = require('passport');
 var baucis          = require('baucis');
 var user 			= require('./models/user');
 var cookieParser    = require('cookie-parser');
 
+if (process.env.NODE_ENV === 'production'){
+	console.log('ENV: PROD');
+	mongoose.connect('mongodb://localhost/blog', function (err) {
+		if (err) console.error(err);
+	});
+} else {
+	console.log('ENV: DEV');
+}
 require('./updates/init');
 
 var app = express();
 
 app.use(express.static(__dirname + '/static'));
-app.use(session({ 
+app.use(session({
 	secret: 'topsecret',
     resave: true,
     saveUninitialized: true
