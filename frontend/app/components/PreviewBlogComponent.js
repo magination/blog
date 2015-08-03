c0f8d427269290d99a056226f2d3089ffd44870f
Juul Arthur Ribe Rudihagen <juularthur92@gmail.com>
2015-08-03 15:13:01 -0400

    quickfix in preview to show correctly with html

--- frontend/app/components/PreviewBlogComponent.js
+++ frontend/app/components/PreviewBlogComponent.js
@@ -6,19 +6,24 @@ var previewBlogComponent = React.createClass({
     propTypes: {
         blogpost: React.PropTypes.object
     },
-    render: function(){
+    render: function() {
         //If body is larger than 1000 chars, limit to 1000 and add dotdotdot
-        var body = this.props.blogpost.body.length > 1000 ? this.props.blogpost.body.substring(0, 1000) + "..." : this.props.blogpost.body;
+        //var body = this.props.blogpost.body.length > 1000 ? this.props.blogpost.body.substring(0, 1000) + "..." + "</p>": this.props.blogpost.body;
+
         var author = this.props.blogpost.author ? this.props.blogpost.author.fullname : "Magination";
+
         return (
             <div className="blogPostPreview">
                 <h2> <Link to={'/post/' + this.props.blogpost.slug}> {this.props.blogpost.title} </Link> </h2>
                 <div className="author">Written by: <span className="authorName"> {author} </span></div>
-                <div className="blog-preview-body" dangerouslySetInnerHTML={{__html: body}}/>
+                <div className="blog-preview-body" dangerouslySetInnerHTML={{__html: this.body()}}/>
                 <Link to={'/post/' + this.props.blogpost.slug}><button name="readMoreButton">Read more</button></Link>
                 <hr />
             </div>
         );
+    },
+    body: function() {
+        return this.props.blogpost.body.split("</p>")[0] + "...</p>";
     }
 });
 
